155382778b9d1ac9e703720e90be12f584a8a011
LarissaFiuza7 <123764342+LarissaFiuza7@users.noreply.github.com>
2023-11-13 02:05:53 +0000

    Adicionando a função alterar tarefa, que tem como objetivo modificar uma tarefa já existente

--- biblioteca.h
+++ biblioteca.h
@@ -1,11 +1,12 @@
 #ifndef PROJET_1_BIBLIOTECA_H
 #define PROJET_1_BIBLIOTECA_H
-
+#include <string.h>
 // Cria a estrutura de tarefa, dando 3 parâmetros, priroidade, categoria e descrição
 typedef struct {
   int prioridade;
   char categoria[100];
   char descricao[300];
+  char estado[20];
 } Tarefa;
 
 // Cria a lista de tarefas
@@ -20,5 +21,6 @@ int deletarTarefa(ListaDeTarefas *lt, int indice);
 void listarTarefas(ListaDeTarefas lt);
 void salvarLista(ListaDeTarefas lt, const char *tarefas);
 void carregarLista(ListaDeTarefas *lt, const char *tarefas);
+void alterar_Tarefa(ListaDeTarefas*lt, int indice);
 
 #endif // PROJET_1_BIBLIOTECA_H
